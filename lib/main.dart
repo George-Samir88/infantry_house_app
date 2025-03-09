defb09ac8b52e4be4c672aa8f6b1ff10cd05d7c4
George-samir88 <George-samir88@gmail.com>
2025-03-09 11:41:44 +0200

    test commit4

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _InfantryHouseAppState extends State<InfantryHouseApp> {
       _locale = locale;
     });
   }
-///changes  test commit
+///changes
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
