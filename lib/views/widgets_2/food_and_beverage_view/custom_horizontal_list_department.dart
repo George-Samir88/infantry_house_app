1a9c051864507fefb1f7d88c3a6e946d7d14a718
George-samir88 <George-samir88@gmail.com>
2025-03-10 16:49:02 +0200

    changing button position of screen department edit

--- lib/views/widgets_2/food_and_beverage_view/custom_horizontal_list_department.dart
+++ lib/views/widgets_2/food_and_beverage_view/custom_horizontal_list_department.dart
@@ -30,82 +30,111 @@ class _CustomHorizontalListDepartmentState
             if (cubit.newScreensMap.isNotEmpty)
               Container(
                 margin: EdgeInsets.only(left: 16.w, right: 16.w),
-                height: GlobalData().isTabletLayout ? 50.h : 40.h,
+                height: GlobalData().isTabletLayout ? 60.h : 40.h,
                 // Adjust height as needed
-                child: ListView.separated(
-                  clipBehavior: Clip.none,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  // Number of buttons
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ListView.separated(
+                        clipBehavior: Clip.none,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: categories.length,
+                        // Number of buttons
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              cubit.selectedButtonCategoryIndex = index;
+                              setState(() {});
+                              cubit.changeSelectedScreen(
+                                buttonCategoryTitle: categories[index],
+                              );
+                            },
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.shade400,
+                                    // Shadow color with opacity
+                                    spreadRadius: 1,
+                                    // Spread area of the shadow
+                                    blurRadius: 10,
+                                    // Blur effect
+                                    offset: Offset(
+                                      0,
+                                      3,
+                                    ), // Changes position of shadow (X, Y)
+                                  ),
+                                ],
+                                color:
+                                    cubit.selectedButtonCategoryIndex == index
+                                        ? Colors.brown[800]
+                                        : Color(0xffFAF7F0),
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  categories[index],
+                                  style: TextStyle(
+                                    color:
+                                        cubit.selectedButtonCategoryIndex ==
+                                                index
+                                            ? Color(0xffFFFFFF)
+                                            : Color(0xff5E3D2E),
+                                    fontSize:
+                                        GlobalData().isTabletLayout
+                                            ? (cubit.selectedButtonCategoryIndex ==
+                                                    index
+                                                ? 10.sp
+                                                : 8.sp)
+                                            : cubit.selectedButtonCategoryIndex ==
+                                                index
+                                            ? 14.sp
+                                            : 12.sp,
+                                    fontWeight:
+                                        cubit.selectedButtonCategoryIndex ==
+                                                index
+                                            ? FontWeight.w500
+                                            : null,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return SizedBox(width: 12.w);
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 12.w),
+                    CustomEditButton(
+                      iconSize: GlobalData().isTabletLayout ? 40.r : null,
+                      height: GlobalData().isTabletLayout ? 50.h : null,
+                      width: GlobalData().isTabletLayout ? 30.w : null,
+                      iconColor: Colors.black,
+                      backgroundColor: Colors.grey.shade200,
                       onTap: () {
-                        cubit.selectedButtonCategoryIndex = index;
-                        setState(() {});
-                        cubit.changeSelectedScreen(
-                          buttonCategoryTitle: categories[index],
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                (context) => BlocProvider.value(
+                                  value: cubit,
+                                  child: EditScreenDepartmentView(),
+                                ),
+                          ),
                         );
                       },
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.shade400,
-                              // Shadow color with opacity
-                              spreadRadius: 1,
-                              // Spread area of the shadow
-                              blurRadius: 10,
-                              // Blur effect
-                              offset: Offset(
-                                0,
-                                3,
-                              ), // Changes position of shadow (X, Y)
-                            ),
-                          ],
-                          color:
-                              cubit.selectedButtonCategoryIndex == index
-                                  ? Colors.brown[800]
-                                  : Color(0xffFAF7F0),
-                          borderRadius: BorderRadius.circular(14),
-                        ),
-                        child: Center(
-                          child: Text(
-                            categories[index],
-                            style: TextStyle(
-                              color:
-                                  cubit.selectedButtonCategoryIndex == index
-                                      ? Color(0xffFFFFFF)
-                                      : Color(0xff5E3D2E),
-                              fontSize:
-                                  GlobalData().isTabletLayout
-                                      ? (cubit.selectedButtonCategoryIndex ==
-                                              index
-                                          ? 10.sp
-                                          : 8.sp)
-                                      : cubit.selectedButtonCategoryIndex ==
-                                          index
-                                      ? 14.sp
-                                      : 12.sp,
-                              fontWeight:
-                                  cubit.selectedButtonCategoryIndex == index
-                                      ? FontWeight.w500
-                                      : null,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return SizedBox(width: 12.w);
-                  },
+                      icon: Icons.edit,
+                    ),
+                  ],
                 ),
               ),
-            if(cubit.newScreensMap.isEmpty)
+            if (cubit.newScreensMap.isEmpty)
               Center(
                 child: Text(
                   S.of(context).LaYogdAksam,
@@ -114,30 +143,6 @@ class _CustomHorizontalListDepartmentState
                   ),
                 ),
               ),
-            Positioned(
-              left: 10,
-              bottom: -5,
-              child: CustomEditButton(
-                height: GlobalData().isTabletLayout ? 50.h : 30.h,
-                width: GlobalData().isTabletLayout ? 30.w : 30.w,
-                iconSize: GlobalData().isTabletLayout ? 40.r : 20.r,
-                iconColor: Colors.black,
-                backgroundColor: Colors.grey.shade200,
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder:
-                          (context) => BlocProvider.value(
-                            value: cubit,
-                            child: EditScreenDepartmentView(),
-                          ),
-                    ),
-                  );
-                },
-                icon: Icons.edit,
-              ),
-            ),
           ],
         );
       },
