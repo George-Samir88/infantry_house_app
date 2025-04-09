ed17e7e6c9224e0b79e358e4060aae10bbb3084a
George-Samir88 <George-Samir88@gmail.com>
2025-04-09 22:11:36 +0200

    some refactor of activities view

--- lib/views/widgets/activities_view/activities_view_body.dart
+++ lib/views/widgets/activities_view/activities_view_body.dart
@@ -1,21 +1,14 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:infantry_house_app/utils/custom_app_bar.dart';
-import 'package:infantry_house_app/views/widgets/activities_view/A_edit_carousel_template.dart';
-import 'package:infantry_house_app/views/widgets/activities_view/subscription_view/subscriptions_view_body.dart';
 
 import '../../../generated/l10n.dart';
-import '../../../global_variables.dart';
-import '../../../utils/empty_carousel_item.dart';
-import '../../../utils/custom_edit_button.dart';
 import '../../../utils/dots_indicator.dart';
 import '../cart_view/my_cart_view.dart';
 import '../home_view/manager/home_cubit.dart';
-import 'custom_activity_card_item.dart';
-import 'daily_games_view/daily_games_view.dart';
+import 'custom_carousel_slider.dart';
+import 'horizontal_list_of_activities_types.dart';
 import 'manager/activity_cubit.dart';
 
 class ActivitiesViewBody extends StatefulWidget {
@@ -72,75 +65,7 @@ class _ActivitiesViewBodyState extends State<ActivitiesViewBody> {
                   },
                 ),
                 SizedBox(height: 40.h),
-                Stack(
-                  clipBehavior: Clip.none,
-                  children: [
-                    if (activityCubit.carouselItems.isNotEmpty) ...[
-                      Padding(
-                        padding: EdgeInsets.only(left: 10.w, right: 10.w),
-                        child: CarouselSlider.builder(
-                          itemCount: activityCubit.carouselItems.length,
-                          itemBuilder:
-                              (context, index, realIndex) =>
-                                  activityCubit.carouselItems[index],
-                          options: CarouselOptions(
-                            onPageChanged: (index, other) {
-                              activityCubit.changeCarouselIndex(index: index);
-                            },
-                            height: GlobalData().isTabletLayout ? 280.h : 180.h,
-                            clipBehavior: Clip.none,
-                            padEnds: true,
-                            enlargeCenterPage: true,
-                            viewportFraction: 1.2,
-                            enableInfiniteScroll: true,
-                            autoPlay: true,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        left: GlobalData().isArabic ? 5.w : null,
-                        right: GlobalData().isArabic ? null : 5.w,
-                        bottom: -20.h,
-                        child: CustomEditButton(
-                          iconColor: Colors.brown[800],
-                          backgroundColor: Colors.amberAccent.shade100,
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder:
-                                    (context) => BlocProvider.value(
-                                      value: activityCubit,
-                                      child: ActivityEditCarouselTemplateView(),
-                                    ),
-                              ),
-                            );
-                          },
-                          icon: Icons.edit,
-                        ),
-                      ),
-                    ],
-                    if (activityCubit.carouselItems.isEmpty) ...[
-                      Padding(
-                        padding: EdgeInsets.only(left: 10.w, right: 10.w),
-                        child: EmptyCarouselContainer(
-                          onTab: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder:
-                                    (context) => BlocProvider.value(
-                                      value: activityCubit,
-                                      child: ActivityEditCarouselTemplateView(),
-                                    ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ],
-                ),
+                CustomCarouselSlider(activityCubit: activityCubit),
                 SizedBox(height: 20.h),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -152,50 +77,9 @@ class _ActivitiesViewBodyState extends State<ActivitiesViewBody> {
                   ],
                 ),
                 SizedBox(height: 30.h),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.0.w),
-                  child: AnimationLimiter(
-                    child: Row(
-                      children: List.generate(
-                        2,
-                        (index) => Expanded(
-                          child: AnimationConfiguration.staggeredList(
-                            position: index,
-                            duration: const Duration(milliseconds: 1000),
-                            child: SlideAnimation(
-                              verticalOffset: 50.0,
-                              child: FadeInAnimation(
-                                child: GestureDetector(
-                                  onTap: () {
-                                    index == 0
-                                        ? Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder:
-                                                (context) =>
-                                                    SubscriptionsViewBody(),
-                                          ),
-                                        )
-                                        : Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder:
-                                                (context) => DailyGamesView(),
-                                          ),
-                                        );
-                                  },
-                                  child: CustomActivityCardItem(
-                                    activityTitle: activityTitle[index],
-                                    activityImage: activityImages[index],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
+                HorizontalListOfActivitiesTypes(
+                  activityTitle: activityTitle,
+                  activityImages: activityImages,
                 ),
                 SizedBox(height: 20.h),
               ],
---
+++ lib/views/widgets/activities_view/horizontal_list_of_activities_types.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:infantry_house_app/views/widgets/activities_view/subscription_view/subscriptions_view_body.dart';
+
+import 'custom_activity_card_item.dart';
+import 'daily_games_view/daily_games_view.dart';
+
+class HorizontalListOfActivitiesTypes extends StatelessWidget {
+  const HorizontalListOfActivitiesTypes({
+    super.key,
+    required this.activityTitle,
+    required this.activityImages,
+  });
+
+  final List<String> activityTitle;
+  final List<String> activityImages;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0.w),
+      child: AnimationLimiter(
+        child: Row(
+          children: List.generate(
+            2,
+            (index) => Expanded(
+              child: AnimationConfiguration.staggeredList(
+                position: index,
+                duration: const Duration(milliseconds: 1000),
+                child: SlideAnimation(
+                  verticalOffset: 50.0,
+                  child: FadeInAnimation(
+                    child: GestureDetector(
+                      onTap: () {
+                        index == 0
+                            ? Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SubscriptionsViewBody(),
+                              ),
+                            )
+                            : Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DailyGamesView(),
+                              ),
+                            );
+                      },
+                      child: CustomActivityCardItem(
+                        activityTitle: activityTitle[index],
+                        activityImage: activityImages[index],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
