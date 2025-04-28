59625dd30c12579a427821d00954b367301f2c46
George-Samir88 <George-Samir88@gmail.com>
2025-04-28 18:01:11 +0200

    upload to drive again

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,14 +41,6 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
-
     signingConfigs {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as String
@@ -57,6 +49,14 @@ android {
             storePassword = keystoreProperties["storePassword"] as String
         }
     }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+
 }
 
 flutter {
